2fc744e6c57eab765c0376f0d8795f77f4bb2cbe
Ryan Wang <22625346+ryawa@users.noreply.github.com>
2024-01-22 15:11:48 +0000

    Update prefixes and bot messages

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import { Client, Events, GatewayIntentBits, PermissionsBitField } from "discord.js";
+import {
+  Client,
+  Events,
+  GatewayIntentBits,
+  PermissionsBitField,
+} from "discord.js";
 import "./keepalive.js";
 
 const client = new Client({
@@ -10,25 +15,31 @@ const client = new Client({
   ],
 });
 
-client.once(Events.ClientReady, readyClient => {
+// Lock channels
+// Timeout/kick users
+// Invite users
+// Command handling
+// Display name
+
+client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 client.on(Events.MessageCreate, (message) => {
-  if (message.content.startsWith("zap ")) {
+  if (
+    message.content.startsWith("zap ") ||
+    message.content.startsWith("zap! ")
+  ) {
     const author = message.member;
     const member = message.mentions.members.first();
-
-    if (!author.permissions.has(PermissionsBitField.Flags.BanMembers)) {
-      return message.channel.send("Invalid permissions");
-    }
 
     if (!member) {
       return message.channel.send("Invalid member");
     }
 
     if (member.id == author.id || member.id == message.client.user.id) {
-      return message.channel.send(":skull:");
+      message.channel.send(":skull: don't try me");
+      return message.channel.send("https://tenor.com/bZNxS.gif");
     }
 
     if (
@@ -36,17 +47,22 @@ client.on(Events.MessageCreate, (message) => {
       !member.bannable ||
       member.user.bot
     ) {
-      return message.channel.send("Can't ban that user");
+      return message.channel.send("Can't zap that user");
+    }
+
+    if (!author.permissions.has(PermissionsBitField.Flags.BanMembers)) {
+      return message.channel.send("Invalid permissions");
     }
 
     member
       .ban()
       .then((res) => {
         message.channel.send(`Zapped ${res.user.username}! :zap:`);
+        console.log(`${author.user.username} banned ${res.user.username}`);
       })
       .catch((e) => {
         message.channel.send("An error occurred");
-        console.log(e);
+        console.error(e);
       });
   }
 });
